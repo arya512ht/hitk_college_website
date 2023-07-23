705f8254748d0bc4da4fefebc5925c08f75b22d2
Arya Das <arya.das512ht@gmail.com>
2023-07-23 16:05:24 +0530

    second commit

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,4 +1,3 @@
-// user-controller.js
 import User from "../schema/user-schema.js";
 
 export const getUsersData = async (req, res) => {
@@ -18,5 +17,18 @@ export const getUsers = async (request, response) => {
         response.status(404).json({ message: error.message });
     }
 }
+
+export const checkUsernameAvailability = async (req, res) => {
+    try {
+        const existingUser = await User.findOne({ uname: req.body.uname });
+        if (existingUser) {
+            res.json({ available: false });
+        } else {
+            res.json({ available: true });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
 
 
--- server/router/route.js
+++ server/router/route.js
@@ -1,10 +1,7 @@
-
-import express, { Router } from 'express';
-import {getUsers, getUsersData} from '../controller/user-controller.js';
+import express from 'express';
+import { getUsers, getUsersData, checkUsernameAvailability } from '../controller/user-controller.js';
 import multer from 'multer'
 import User from '../schema/user-schema.js'
-
-
 
 const router = express.Router();
 
@@ -48,9 +45,9 @@ router.post('/add', upload.single('image'), async (req, res) => {
 });
 
 router.get('/all', getUsers);
-
 router.post('/getUser', getUsersData);
 
-
+// New route to check the availability of the username
+router.post('/checkUsername', checkUsernameAvailability);
 
 export default router;
